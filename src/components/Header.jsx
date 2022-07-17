2b0463089f82cef5a55c0e9212ce00ce75451806
satyamkushwaha <satyamkushwaha1101@gmail.com>
2022-07-18 00:59:33 +0530

    navbar in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,126 +3,143 @@ import React from 'react'
 function Header() {
     return (
         <>
-
-            <header >
-                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 border-2">
-                    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                        <a href="#" className="flex items-center">
-                            <img
-                                src="https://flowbite.com/docs/images/logo.svg"
-                                className="mr-3 h-6 sm:h-9"
-                                alt="Flowbite Logo"
-                            />
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                                Flowbite
-                            </span>
-                        </a>
-                        <div className="flex items-center lg:order-2">
-                            <a
-                                href="#"
-                                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                            >
-                                Log in
-                            </a>
-                            <a
-                                href="#"
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            >
-                                Get started
-                            </a>
-                            <button
-                                data-collapse-toggle="mobile-menu-2"
-                                type="button"
-                                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                aria-controls="mobile-menu-2"
-                                aria-expanded="false"
-                            >
-                                <span className="sr-only">Open main menu</span>
-                                <svg
-                                    className="w-6 h-6"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                                <svg
-                                    className="hidden w-6 h-6"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                        <div
-                            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                            id="mobile-menu-2"
-                        >
-                            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white"
-                                        aria-current="page"
-                                    >
-                                        Home
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Company
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Marketplace
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Features
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Team
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Contact
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+            <div className='h-[5rem]   rounded-sm flex justify-between items-center w-full border-b-slate-100 border-b-2'>
+                <div className='w-2/12  flex justify-center'>
+                    <div className=''>
+                        <img src="/icons/our-batch-logo.png" alt="" />
                     </div>
-                </nav>
-            </header>
+                </div>
+                <div className='w-6/12 flex px-20'>
+                    <label htmlFor="simple-search" className="sr-only">
+                        Search
+                    </label>
+                    <div className="relative w-full">
+                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg
+                                aria-hidden="true"
+                                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                        <input
+                            type="text"
+                            id="simple-search"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Search"
+                            required=""
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        <svg
+                            className="w-5 h-5"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+
+
+                </div>
+                <div className='w-4/12 flex justify-end px-2'>
+                    <div className=''>
+
+                        <button
+                            id="dropdownUserAvatarButton"
+                            data-dropdown-toggle="dropdownAvatar"
+                            className=" flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            type="button"
+                        >
+                            <span className="sr-only">Open user menu</span>
+                            <img
+                                className="w-10 h-10 rounded-full"
+                                src="/images/men.jpg"
+                                alt="user photo"
+                            />
+                        </button>
+                    </div>
+                    {/* <div>
+                        ss
+                    </div> */}
+
+                    {/* Dropdown menu */}
+                    {/* <div
+                        id="dropdownAvatar"
+                        className="z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 block"
+                        data-popper-reference-hidden=""
+                        data-popper-escaped=""
+                        data-popper-placement="bottom"
+                        style={{
+                            position: "absolute",
+                            inset: "0px auto auto 0px",
+                            margin: 0,
+                            transform: "translate3d(326.4px, 62.4px, 0px)"
+                        }}
+                    >
+                        <div className="py-3 px-4 text-sm text-gray-900 dark:text-white">
+                            <div>Bonnie Green</div>
+                            <div className="font-medium truncate">name@flowbite.com</div>
+                        </div>
+                        <ul
+                            className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="dropdownUserAvatarButton"
+                        >
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    Settings
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    Earnings
+                                </a>
+                            </li>
+                        </ul>
+                        <div className="py-1">
+                            <a
+                                href="#"
+                                className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                            >
+                                Sign out
+                            </a>
+                        </div>
+                    </div> */}
+
+                </div>
+            </div>
         </>
 
     )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,81 +3,11 @@ import React, { useState } from 'react'
 function Sidebar() {
     const [open,setOpen]=useState(true)
     return (
-        <>
+        <div>
             {/* component */}
-           
-            <span
-                className="absolute text-white text-4xl top-5 left-4 cursor-pointer"
-                // onclick="openSidebar()"
-
-            >
-                <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md" />
-            </span>
-            <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
-                <div className="text-gray-100 text-xl">
-                    <div className="p-2.5 mt-1 flex items-center">
-                        <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600" />
-                        <h1 className="font-bold text-gray-200 text-[15px] ml-3">
-                            TailwindCSS
-                        </h1>
-                        <i
-                            className="bi bi-x cursor-pointer ml-28 lg:hidden"
-                            onclick="openSidebar()"
-                        />
-                    </div>
-                    <div className="my-2 bg-gray-600 h-[1px]" />
-                </div>
-                <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
-                    <i className="bi bi-search text-sm" />
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-                    />
-                </div>
-                <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                    <i className="bi bi-house-door-fill" />
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
-                </div>
-                <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                    <i className="bi bi-bookmark-fill" />
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Bookmark</span>
-                </div>
-                <div className="my-4 bg-gray-600 h-[1px]" />
-                <div
-                    className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                    onclick="dropdown()"
-                >
-                    <i className="bi bi-chat-left-text-fill" />
-                    <div className="flex justify-between w-full items-center">
-                        <span className="text-[15px] ml-4 text-gray-200 font-bold">
-                            Chatbox
-                        </span>
-                        <span className="text-sm rotate-180" id="arrow">
-                            <i className="bi bi-chevron-down" />
-                        </span>
-                    </div>
-                </div>
-                <div
-                    className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-                    id="submenu"
-                >
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Social
-                    </h1>
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Personal
-                    </h1>
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Friends
-                    </h1>
-                </div>
-                <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                    <i className="bi bi-box-arrow-in-right" />
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
-                </div>
-            </div>
-        </>
+           Sidebar
+         
+        </div>
 
     )
 }
