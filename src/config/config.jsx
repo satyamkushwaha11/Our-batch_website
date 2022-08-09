e3509049acbbe69f1b07c5054088b3053cae6c2b
satyamkushwaha <satyamkushwaha1101@gmail.com>
2022-08-09 15:33:29 +0530

    add remote api-host

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -3,8 +3,8 @@ export const APP_NAME = 'BLOG-E';
 
 export const APP_LOGO = '/icons/app_Icon.png';
 
-export const API_HOST='http://localhost:7001';
-
+// export const API_HOST='http://localhost:7001';
+export const API_HOST="https://our-batch-backend.herokuapp.com"
 
 
 // cloudinary creds 
